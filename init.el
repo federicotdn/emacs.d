f36db292386fbef24e198c3c3cd8d5423f2d5100
Federico Tedin <federicotedin@gmail.com>
2025-01-06 11:52:20 +0100

    lines

--- init.el
+++ init.el
@@ -26,8 +26,7 @@
       mark-even-if-inactive nil
       project-vc-extra-root-markers '(".project")
       duplicate-line-final-position 1
-      eglot-report-progress nil
-      eglot-ignored-server-capabilities '(:inlayHintProvider)
+      eglot-report-progress nil eglot-ignored-server-capabilities '(:inlayHintProvider)
       warning-minimum-level :error
       org-use-property-inheritance t
       xref-search-program 'ripgrep
